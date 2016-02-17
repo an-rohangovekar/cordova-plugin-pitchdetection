e7e00f643f9d9c77fa41d9b317a23bd43d9eb8b7
an-rohangovekar <rohan.g@applicationnexus.com>
2016-02-17 18:56:15 +0530

    Update RIOInterface.mm

--- src/ios/RIOInterface.mm
+++ src/ios/RIOInterface.mm
@@ -318,9 +318,9 @@ void ConvertInt16ToFloat(RIOInterface* THIS, void *buf, float *outputBuf, size_t
                                kAudioUnitScope_Input,
                                kInputBus, &enableInput, sizeof(enableInput));
     
-    /*err = AudioUnitSetProperty(ioUnit, kAudioOutputUnitProperty_EnableIO,
-     kAudioUnitScope_Output,
-     kOutputBus, &enableOutput, sizeof(enableOutput));*/
+    err = AudioUnitSetProperty(ioUnit, kAudioOutputUnitProperty_EnableIO,
+                                kAudioUnitScope_Output,
+                                kOutputBus, &enableOutput, sizeof(enableOutput));
     
     err = AudioUnitSetProperty(ioUnit, kAudioOutputUnitProperty_SetInputCallback,
                                kAudioUnitScope_Input,
@@ -330,9 +330,9 @@ void ConvertInt16ToFloat(RIOInterface* THIS, void *buf, float *outputBuf, size_t
     // Set the stream format.
     size_t bytesPerSample = [self ASBDForSoundMode];
     
-    /* err = AudioUnitSetProperty(ioUnit, kAudioUnitProperty_StreamFormat,
+     err = AudioUnitSetProperty(ioUnit, kAudioUnitProperty_StreamFormat,
      kAudioUnitScope_Output,
-     kInputBus, &streamFormat, sizeof(streamFormat));*/
+     kInputBus, &streamFormat, sizeof(streamFormat));
     
     err = AudioUnitSetProperty(ioUnit, kAudioUnitProperty_StreamFormat,
                                kAudioUnitScope_Input,
@@ -343,7 +343,9 @@ void ConvertInt16ToFloat(RIOInterface* THIS, void *buf, float *outputBuf, size_t
     
     // Disable system buffer allocation. We'll do it ourselves.
     UInt32 flag = 0;
-    //err = AudioUnitSetProperty(ioUnit, kAudioUnitProperty_ShouldAllocateBuffer, kAudioUnitScope_Output, kInputBus, &flag, sizeof(flag));
+    err = AudioUnitSetProperty(ioUnit, kAudioUnitProperty_ShouldAllocateBuffer, 
+                                kAudioUnitScope_Output, 
+                                kInputBus, &flag, sizeof(flag));
     
     
     // Allocate AudioBuffers for use when listening.
